e52564a40e1da9bd56a8edbe3fdd9bb783983411
Ricard0Brites <ricardo.brites.work@gmail.com>
2022-03-06 19:34:00 +0000

    DataRequest.cs Code Clean-up

--- DB Control Logic/DataRequest.cs
+++ DB Control Logic/DataRequest.cs
@@ -11,11 +11,6 @@ namespace Book_Kepping.DB_Control_Logic
 {
     public class DataRequest
     {
-        //Pull From the Database 
-        //connection.Query<>()
-
-
-        //Push Requests to the database
         public static bool InsertBookKeepingEntry( bool movementType, string transactionDescription, float transactionAmmount, DateTime date, string supportingFilesPath)
         {
             //makes the true or false become 1 or 0
@@ -24,12 +19,8 @@ namespace Book_Kepping.DB_Control_Logic
             {
                 movementType2 = 1;
             }
-            else
-            {
-                movementType2 = 0;
-            } 
 
-            //connects to the database and writes the given data
+            //connects to the database and writes data
             if(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName()) != null)
             {
                 using (IDbConnection connection = new SqlConnection(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName())))
@@ -45,11 +36,13 @@ namespace Book_Kepping.DB_Control_Logic
                         id = 0;
                     }
                     connection.Execute($"INSERT INTO [{Database_Info.GetDatabaseName()}].[dbo].{Database_Info.GetCashMethodTableName()} (ID, Movement_Type, [Description], Amount, [Date], [Supporting_Evidence_Path]) VALUES ({id + 1}, {movementType2}, '{transactionDescription}', ({transactionAmmount}), '{date.Year + date.Month + date.Day}', '{supportingFilesPath}');");
+                    GC.Collect();
                     return true;
                 }
             }
             else
             {
+                GC.Collect();
                 return false;
             }
         }
@@ -58,45 +51,41 @@ namespace Book_Kepping.DB_Control_Logic
         {
             if(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName()) != null)
             {
-                using (IDbConnection connection = new SqlConnection(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName())))
+                //database connection
+                using IDbConnection connection = new SqlConnection(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName()));
+
+                //creates table, inserts data, gets data and drops table
+                connection.Execute($"CREATE TABLE [{Database_Info.GetDatabaseName()}].[dbo].TestTable(id int); INSERT INTO [{Database_Info.GetDatabaseName()}].[dbo].TestTable (id) VALUES (1), (2), (3);");
+                var dataReturnedByQuery = connection.Query<int>($"select * from [{Database_Info.GetDatabaseName()}].[dbo].TestTable;");
+                connection.Execute($"DROP TABLE [{Database_Info.GetDatabaseName()}].[dbo].TestTable;");
+
+                //verifies that the connection is working
+                if (dataReturnedByQuery.ElementAt(0) == 1 && dataReturnedByQuery.ElementAt(1) == 2 && dataReturnedByQuery.ElementAt(2) == 3)
                 {
-                    connection.Execute($"CREATE TABLE [{Database_Info.GetDatabaseName()}].[dbo].TestTable(id int); INSERT INTO [{Database_Info.GetDatabaseName()}].[dbo].TestTable (id) VALUES (1), (2), (3);");
-                    var payload = connection.Query<int>($"select * from [{Database_Info.GetDatabaseName()}].[dbo].TestTable;");
-                    connection.Execute($"DROP TABLE [{Database_Info.GetDatabaseName()}].[dbo].TestTable;");
-                    if (payload.ElementAt(0) == 1 && payload.ElementAt(1) == 2 && payload.ElementAt(2) == 3)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
+                    GC.Collect();
+                    return true;
                 }
             }
-            else
-            {
-                return false;
-            }
+            GC.Collect();
+            return false;
         }
 
         public static int GetLastCashID()
         {
             if (DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName()) != null)
             {
-                using (IDbConnection connection = new SqlConnection(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName())))
+                using IDbConnection connection = new SqlConnection(DBConnectionStringHelper.ConnectionValue(Database_Info.GetDatabaseName()));
+                int id = 0;
+                try
                 {
-                    int id = 0;
-                    try
-                    {
-                        var idt = connection.Query<int>($"SELECT MAX(ID)FROM [{Database_Info.GetDatabaseName()}].[dbo].{Database_Info.GetCashMethodTableName()};");
-                        id = idt.First();
-                    }
-                    catch (Exception)
-                    {
-                        id = 0;
-                    }
-                    return id;
+                    var queryResults = connection.Query<int>($"SELECT MAX(ID)FROM [{Database_Info.GetDatabaseName()}].[dbo].{Database_Info.GetCashMethodTableName()};");
+                    id = queryResults.First();
                 }
+                catch (Exception)
+                {
+                    id = 0;
+                }
+                return id;
             }
             return -1;
         }
